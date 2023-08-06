db5727bf7a1d13ebcc8e09f20423ceaef2e2755a
Raviteja <raviteja.karri@chromiumsolutions.com>
2023-08-06 09:51:35 +0530

    first commit

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: true }));
 // });
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! from server');
 });
 
 app.post("/img_answering", async (req, res) => {
